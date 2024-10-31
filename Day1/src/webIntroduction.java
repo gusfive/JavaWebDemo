44bff04477d4204d9f818aa6a73a398a2d4ddd10
gusfive <gushang5@foxmail.com>
2024-10-31 19:59:12 +0800

    day1

---
+++ Day1/src/webIntroduction.java
@@ -0,0 +1,24 @@
+public class webIntroduction {
+    public static void main(String[] args) {
+//        web网站工作流程
+//          浏览器向前端发送请求，前端服务器响应浏览器，浏览器从响应信息找到并访问后端服务器地址
+//          后端服务器向数据库发送请求，数据库响应后端服务器，后端服务器将数据库的响应数据返回给浏览器
+//        前端Web开发技术栈：HTML、CSS、JavaScript  Vue、Element、Nginx
+//        后端Web开发技术栈：Maven SpringBoot-Web MySQL SpringBoot Mybatis
+
+//        初识web前端
+//          前端的代码是通过浏览器转化（解析和渲染）成用户看到的网页
+//          浏览器中对代码进行解析渲染的部分，称为浏览器内核
+//          web标准
+//              也称为网页标准，由一系列的标准组成，大部分由w3c（World Wide Web Consortium,万维网联盟）负责制定
+//              三个组成部分：
+//                  HTML：负责网页的结构（页面元素和内容）
+//                  CSS：负责网页的表现（页面元素的外观、位置等页面样式，如颜色大小等）
+//                  JavaScript:负责网页的行为（交互效果）
+//        HTML
+//          HTML(HyperText Markup Language):超文本标记语言
+//
+
+    }
+
+}
