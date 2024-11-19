c34e0a7d6ca2253003a1227ebf28f8c6d4b14cd9
gusfive <gushang5@foxmail.com>
2024-11-19 16:27:27 +0800

    new

---
+++ springboot-web-quickstart/src/test/java/com/gus/SpringbootWebQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
